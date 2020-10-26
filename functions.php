99094ec369cc702d7ab727d42abb120f22f72b36
mozinomer <mozinomer12@gmail.com>
2020-10-26 10:15:36 +0500

    asd

--- functions.php
+++ functions.php
@@ -51,3 +51,14 @@ function Gallery_postType() {
     );
 }
 add_action( 'init', 'Gallery_postType' );
+
+
+function register_my_menus() {
+  register_nav_menus(
+    array(
+      'header-menu' => __( 'Header Menu' ),
+      'extra-menu' => __( 'Extra Menu' )
+    )
+  );
+}
+add_action( 'init', 'register_my_menus' );
